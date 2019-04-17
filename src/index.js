a2f2c2aef082926ebcc7d6920931f5ecc30b9306
Abhijitno1 <abhijitno1@yahoo.com>
2019-04-17 21:14:55 +0530

    First changes successful

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 /* eslint-disable no-console */
-console.log('hi from Abhijit');
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'; //Webpack can import CSS files too!
+
+render(<h1>React and Redux in ES6 Derived from Pluralsight Course</h1>, document.getElementById('app'));
