fa84f5cd4ba4e19ee7650ef360530019997453d9
Abhijitno1 <abhijitno1@yahoo.com>
2019-04-17 22:59:18 +0530

    leftover from previous commit

---
+++ src/components/common/headerControl.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HeaderControl = () => {
+    return (
+        <nav>
+            <a href="/#home">Home</a>
+            &nbsp;| <a href="/#courses">Courses</a>
+            &nbsp;| <a href="/#about">About</a>
+        </nav>
+    );
+};
+
+export default HeaderControl;
